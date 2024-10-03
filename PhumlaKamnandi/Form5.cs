7a752f22b1cea30fe92f467260312dad470ec051
4z00r <azharsalie1305@gmail.com>
2024-10-04 00:03:14 +0200

    started working on summary + enquiry functionality (not fully implemented yet )

--- PhumlaKamnandi/Form5.cs
+++ PhumlaKamnandi/Form5.cs
@@ -1,6 +1,7 @@
 ﻿using PhumlaKamnandi.Business;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -22,22 +23,55 @@ namespace PhumlaKamnandi
         {
             this.hotel = hotel;
             InitializeComponent();
+            bookingController = new BookingController();
+            //hotel.
+             
         }
         private void setUpListView()
         {
-            ListViewItem bookingDetails;
-            Booking b; 
-            bookingListView.Clear();
+            //ListViewItem bookingDetails;
+            //Booking b; 
+            //bookingListView.Clear();
 
-            bookingListView.Columns.Insert(0, "BookingID", 120, HorizontalAlignment.Left);
-            bookingListView.Columns.Insert(1, "RoomID", 120, HorizontalAlignment.Left);
-            bookingListView.Columns.Insert(2, "CheckIn", 120, HorizontalAlignment.Left);
-            bookingListView.Columns.Insert(3, "CheckOut", 120, HorizontalAlignment.Left);
+            //bookingListView.Columns.Insert(0, "BookingID", 120, HorizontalAlignment.Left);
+            //bookingListView.Columns.Insert(1, "RoomID", 120, HorizontalAlignment.Left);
+            //bookingListView.Columns.Insert(2, "CheckIn", 120, HorizontalAlignment.Left);
+            //bookingListView.Columns.Insert(3, "CheckOut", 120, HorizontalAlignment.Left);
 
             
-            //foreach(Booking)
-            bookingListView.Refresh();
+            ////foreach(Booking)
+            //bookingListView.Refresh();
+
+
+
             bookingListView.GridLines = true;
+            bookingListView.View = View.Details;
+            bookingListView.FullRowSelect = true;
+            bookingListView.GridLines = true;
+
+            bookingListView.Columns.Add("Booking ID", 100, HorizontalAlignment.Left);
+            bookingListView.Columns.Add("Room ID", 100, HorizontalAlignment.Left);
+            bookingListView.Columns.Add("Check-In", 150, HorizontalAlignment.Left);
+            bookingListView.Columns.Add("Check-Out", 150, HorizontalAlignment.Left);
+            bookingListView.Columns.Add("Price", 100, HorizontalAlignment.Left);
+        }
+
+        private void loadData()
+        {
+            // i don't know how to display just a singular guest's bookings - as a guest isn't associated with a booking?
+            // did it for all bookings *for now* 
+            Collection<Booking> allBookings = bookingController.AllBookings;
+
+            foreach (Booking booking in allBookings)
+            {
+                ListViewItem item = new ListViewItem(booking.BookingID.ToString());
+                item.SubItems.Add(booking.Room.RoomID.ToString());
+                item.SubItems.Add(booking.Dates.CheckIn.ToShortDateString());
+                item.SubItems.Add(booking.Dates.CheckOut.ToShortDateString());
+                item.SubItems.Add(booking.Pricing.Total.ToString("C"));
+
+                bookingListView.Items.Add(item);
+            }
         }
         private void ExitButton_Click(object sender, EventArgs e)
         {
